136f2395c028ae09f59d0eb9e3d17285e7e69c28
Gleb Dunaev~ <7-v-7@bk.ru~>
2022-07-12 21:05:33 +0300

    3.1.3. заменяет тег а на Link.

--- project/src/pages/login-page/login-page.tsx
+++ project/src/pages/login-page/login-page.tsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom';
 
 function LoginPage(): JSX.Element{
 
@@ -12,9 +13,9 @@ function LoginPage(): JSX.Element{
           <div className="container">
             <div className="header__wrapper">
               <div className="header__left">
-                <a className="header__logo-link" href="#">
+                <Link className="header__logo-link" to="#">
                   <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -38,9 +39,9 @@ function LoginPage(): JSX.Element{
             </section>
             <section className="locations locations--login locations--current">
               <div className="locations__item">
-                <a className="locations__item-link" href="#">
+                <Link className="locations__item-link" to="#">
                   <span>Amsterdam</span>
-                </a>
+                </Link>
               </div>
             </section>
           </div>
--- project/src/pages/not-found-page/not-found-page.tsx
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -1,11 +1,12 @@
 import {AppRoute} from '../../const';
+import {Link} from 'react-router-dom';
 
 function NotFoundPage(): JSX.Element{
 
   return (
     <section>
       <h1>404. Page not found</h1>
-      <a href={AppRoute.Main}>Вернуться на главную</a>
+      <Link to={AppRoute.Main}>Вернуться на главную</Link>
     </section>
   );
 }
