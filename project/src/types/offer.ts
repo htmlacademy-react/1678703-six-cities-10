924b98ae3a400868debcfbdc4b6fc3532af50f29
Gleb Dunaev~ <7-v-7@bk.ru~>
2022-07-16 16:44:26 +0300

    4.1.1. добавляет моки офферов

---
+++ project/src/types/offer.ts
@@ -0,0 +1,38 @@
+
+export type Location = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+};
+
+export type Host = {
+  avatarUrl: string;
+  id: number;
+  isPro: boolean;
+  name: string;
+};
+
+export type City = {
+  location: Location;
+  name: string;
+};
+
+export type Offers = {
+  images: string[];
+  goods: string[];
+  location: Location;
+  host: Host;
+  city: City;
+
+  bedrooms: number;
+  description: string;
+  id: number;
+  isFavorite: boolean;
+  isPremium: boolean;
+  maxAdults: number;
+  previewImage: string;
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
+};
