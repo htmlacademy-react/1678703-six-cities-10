5bc554daf973be84314e6dfee45c740eb6fe94b4
Gleb Dunaev~ <7-v-7@bk.ru~>
2022-07-20 21:52:01 +0300

    5.1.1. добавляет зависимости для карты и моки

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -24,8 +24,8 @@ export const offers: Offers = [
     'isFavorite': true,
     'isPremium': true,
     'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.3909553943508,
+      'longitude': 4.85309666406198,
       'zoom': 8
     },
     'maxAdults': 4,
@@ -59,8 +59,8 @@ export const offers: Offers = [
     'isFavorite': true,
     'isPremium': false,
     'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.369553943508,
+      'longitude': 4.85309666406198,
       'zoom': 8
     },
     'maxAdults': 4,
@@ -127,8 +127,8 @@ export const offers: Offers = [
     'isFavorite': false,
     'isPremium': false,
     'location': {
-      'latitude': 52.35514938496378,
-      'longitude': 4.673877537499948,
+      'latitude': 52.3909553943508,
+      'longitude': 4.929309666406198,
       'zoom': 8
     },
     'maxAdults': 4,
